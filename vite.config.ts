235e93164300f67f5332a3996f2868415a2b5473
wallace lentes <wallace.lentes@gmail.com>
2025-07-29 14:28:44 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Lp_petshop/",
+  base: "/Lp_Petshop/", 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-   
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
