422f18a6487685e0aefb1e7b629198c69a065598
fernando <fernando@fanwave.digital>
2025-08-06 21:47:48 -0300

    Change basename to server content on index

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/Lp_Petshop/", 
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "/",
+    server: {
+        host: "::",
+        port: 8080,
     },
-  },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
